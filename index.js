3c8b4bfa0b2f094e1c3d32bda27e8061a843ef20
unknown <clonebmn2itt@gmail.com>
2018-09-29 12:58:26 +0700

    Done !

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
 request = require('request-promise');
 var logIDs = new Array();
+var recentStatus;
 const ACCESS_TOKEN = 'EAAAAUaZA8jlABACqu8s2LZADQQ5BCREfS2AVGDosRIN3xr8cj9majQTxTXGgMh2OtO9K0gFASvEczBxdEiZAplPvvnRc4JtQOZAVFHoKPQLQlayB3ikzzKQHfDsTVjhavW4d80xPJtWHI3eVejTiIEZAZBzdpBKVIJZAaZBUknsJJDfGu2wq9hTS'
-request({
-    url: 'https://api.facebook.com/method/fql.query',
-    qs: {
-        access_token: ACCESS_TOKEN,
-        format: 'json',
-        query: 'SELECT text, fromid, time, object_id, id FROM comment WHERE fromid != me() AND object_id IN (SELECT status_id FROM status WHERE uid = "100009859624773" limit 1)  AND text != "" order by time desc LIMIT 2'
-    },
-    json: true
-})
-    .then(result => {
-        if (result.error_code || result.error_msg) return console.log('Lỗi cmnr! ', result.error_msg) && process.exit(1)
-        //console.log(result);
-
-
-        let i = 0
-        let ObjectID = result[0] && result[0].object_id;
-        while (i < result.length) {
-
-            let j = i;
-            let idCmt = result[j].id;
-            let idUserCmt = result[j].fromid;
-            let contentComment = result[j].text;
-            if (logIDs.includes(idCmt)) break;
-            logIDs.push(idCmt)
-            if (!contentComment) continue;
-            getsimi(contentComment)
-                .then(replysimsimi =>
-                    request({
-                        url: `https://graph.facebook.com/${ObjectID}/comments`,
-                        qs: {
-                            access_token: ACCESS_TOKEN,
-                            message: `@[${idUserCmt}:0] ${replysimsimi || 'Hông biết nói gì'}`,
-                            method: 'POST'
-
-                        }
-                    })
-                ).then(
-                    a => console.log(a)
-                )
-                .catch(error => {
-                    console.log()
-                })
-
-
-            i++;
-        }
-
+var cronjob = setInterval(() => {
+    request({
+        url: 'https://api.facebook.com/method/fql.query',
+        qs: {
+            access_token: ACCESS_TOKEN,
+            format: 'json',
+            query: 'SELECT text, fromid, time, object_id, id FROM comment WHERE fromid != me() AND object_id IN (SELECT status_id FROM status WHERE uid = "100009859624773" limit 1)  AND text != "" order by time desc LIMIT 2'
+        },
+        json: true
     })
-    .catch(e => console.log('Lỗi ' + e))
+        .then(result => {
+            if (result.error_code || result.error_msg) return console.log('Lỗi cmnr! ', result.error_msg) && clearInterval(cronjob) && process.exit(1)
+            //console.log(result);
 
 
-function getsimi(text = 'hi') {
+            let i = 0
+            let ObjectID = result[0] && result[0].object_id;
+            if (recentStatus != ObjectID) {
+                console.log('Đang quét cmt của id', ObjectID);
+                recentStatus = ObjectID
+            }
+            while (i < result.length) {
+
+                let j = i;
+                let idCmt = result[j].id;
+                let idUserCmt = result[j].fromid;
+                let contentComment = result[j].text;
+                if (logIDs.includes(idCmt)) break;
+                logIDs.push(idCmt)
+                if (!contentComment) continue;
+                getsimi(contentComment)
+                    .then(replysimsimi =>
+                        request({
+                            url: `https://graph.facebook.com/${ObjectID}/comments`,
+                            qs: {
+                                access_token: ACCESS_TOKEN,
+                                message: `@[${idUserCmt}:0] ${replysimsimi || 'Hông biết nói gì'}`,
+                                method: 'POST'
+
+                            }
+                        })
+                    ).then(
+                        a => console.log(a)
+                    )
+                    .catch(error => {
+                        console.log('' + error)
+                    })
+
+
+                i++;
+            }
+
+        })
+        .catch(e => console.log('Lỗi ' + e))
+}, 2000)
+function getsimi(text) {
 
     return request({
         url: 'http://api.simsimi.com/request.p',
